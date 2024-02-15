c3153a7060dfa32cc2ac0017e3c55fe8b55a384e
JAN <janstefanlebar@gmail.com>
2024-02-15 17:06:09 +0100

    clients and handyman

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import { Button } from "@/components/ui/button"
- 
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+
 export default function Home() {
   return (
     <div>
+      <Avatar>
+  <AvatarImage src="https://www.shutterstock.com/image-vector/young-smiling-man-avatar-brown-600nw-2261401207.jpg" />
+  <AvatarFallback>CN</AvatarFallback>
+</Avatar>
       <Button>Click me</Button>
     </div>
   )
 }
+
+
