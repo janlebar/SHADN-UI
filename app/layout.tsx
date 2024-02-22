dbc4f6a160503da37cae4a54e8588446148f1a79
JAN <janstefanlebar@gmail.com>
2024-02-22 14:13:58 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import './globalicon.css';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
