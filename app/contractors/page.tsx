29c03f0c3aaf9155cdfc55afcec1d5c8bc986e6f
JAN <janstefanlebar@gmail.com>
2024-03-22 18:29:16 +0100

    change

--- app/contractors/page.tsx
+++ app/contractors/page.tsx
@@ -1,3 +1,19 @@
-export default function contractors() {
-  return <h2>test</h2>
+// import { Button } from "@/components/ui/button"
+// import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import HandymanPortfolioList from "@/components/contractors/contractors"
+import { handymanProfiles } from "@/components/contractors/demo"
+import {Header} from "@/components/header"
+import Headerswitch from "@/components/headerswitch"
+
+
+export default function ContractorsPage() {
+  return (
+    <div>
+      <Header/>
+      <Headerswitch/>
+      <HandymanPortfolioList handymen={handymanProfiles} />
+
+
+    </div>
+  )
 }
