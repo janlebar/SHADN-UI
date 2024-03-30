93c36d4f51560aaacb40b24a234dd7daa43c7568
JAN <janstefanlebar@gmail.com>
2024-03-30 21:50:04 +0100

    map centers correctly

--- components/map.tsx
+++ components/map.tsx
@@ -12,8 +12,10 @@ import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import React, { useState, useEffect } from 'react';
 import LocationComponent from './location'; // Update the path as per your project structure
 
+
+
 export default function Map() {
-  const [userLocation, setUserLocation] = useState({ latitude: 51.505, longitude: -0.09 });
+  const [userLocation, setUserLocation] = useState(null);
 
   useEffect(() => {
     const handleUserLocation = (event) => {
@@ -31,23 +33,25 @@ export default function Map() {
   return (
     <div>
       <LocationComponent />
-      <MapContainer
-        preferCanvas={true}
-        center={[userLocation.latitude, userLocation.longitude]} // Update center with user's location
-        zoom={11}
-        scrollWheelZoom={true}
-        style={{ height: '400px', width: '600px' }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[userLocation.latitude, userLocation.longitude]}>
-          <Popup>
-            This Marker icon is displayed correctly with <i>leaflet-defaulticon-compatibility</i>.
-          </Popup>
-        </Marker>
-      </MapContainer>
+      {userLocation && (
+        <MapContainer
+          preferCanvas={true}
+          center={[userLocation.latitude, userLocation.longitude]}
+          zoom={11}
+          scrollWheelZoom={true}
+          style={{ height: '400px', width: '600px' }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[userLocation.latitude, userLocation.longitude]}>
+            <Popup>
+              This Marker icon is displayed correctly with <i>leaflet-defaulticon-compatibility</i>.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      )}
     </div>
   );
 }
